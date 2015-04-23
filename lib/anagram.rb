978a1e180326f141b8d517bfa657e80ca60ce52f
Guest User <student@epicodus.com>
2015-04-23 16:31:35 -0700

    returns all anagrams or text string if not and anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,17 +2,22 @@ class String
   def anagram(check_words)
     first_word = self.split("").sort()
     second_word = check_words.split(",")
-#    binding.pry# .split('').sort()
 
-  second_word.each do |a|
-    z = a.split('').sort()
-      if first_word.eql?(z)
-        boom = a + " is an anagram of " + self
-      else
-        boom = a + "is not an anagram"
-      end
-      boom
+    anagram_words = []
+    non_anagram_words = []
+
+    second_word.each do |a|
+      z = a.split('').sort()
+        if first_word.eql?(z)
+          anagram_words<<a
+        else
+          non_anagram_words<<a
+        end
     end
-    boom
+    if anagram_words.length == 0
+      "You did not enter an anagram combo"
+    else
+      anagram_words.join(", ")
+    end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,14 +5,14 @@ require "pry"
 
 describe('String#anagram') do
   it('finds if a word is composed of letters from another word') do
-    expect('cat'.anagram('tac')).to(eq("tac is an anagram of cat"))
+    expect('cat'.anagram('tac')).to(eq("tac"))
   end
 
   it('finds if any of the words are anagrams of another word') do
-    expect('heat'.anagram('thea, foot')).to(eq("thea is an anagram of heat"))
+    expect('heat'.anagram('thea, foot')).to(eq("thea"))
   end
 
   it('finds ia any of the words are anagrams of another word') do
-    expect('heat'.anagram('foot')).to(eq(false))
+    expect('heat'.anagram('foot')).to(eq("You did not enter an anagram combo"))
   end
 end
